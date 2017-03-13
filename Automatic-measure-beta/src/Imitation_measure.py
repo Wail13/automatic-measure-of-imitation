6b271236a51307f9d770357e9f9ee68a18234178
wail <Wail13@users.noreply.github.com>
2017-03-13 18:28:40 +0100

    load process data method completed !

--- Automatic-measure-beta/src/Imitation_measure.py
+++ Automatic-measure-beta/src/Imitation_measure.py
@@ -31,7 +31,7 @@ class Imitation:
 #    im=Imitation("walk-simple","walk-complex",256,'C:\\Wail')  im.load_process_data()
     
     
-    def __init__(self,vid1Name,vid2Name,K,projectPath,windowsize=1,skip=1):
+    def __init__(self,vid1Name,vid2Name,K,projectPath,windowsize=1,skip=1,scale=1):
         """
         vid1Name: the name of the video located in the Data folder
         K: number of words in the codebook or the codewords or visual vocabulary or K means number of clusters
@@ -46,11 +46,38 @@ class Imitation:
         self.windowsize=windowsize
         self.projectPath=projectPath
         self.skip=skip
+        self.scale=scale
         
         
         
+    def applypca(data,ncp=None):
+        pca = PCA(n_components=162)
+        X = pca.fit_transform(data)
+
+        expvar=pca.explained_variance_ratio_*100
+
+
+        for i in range(len(expvar)+1):
+            if sum(expvar[:i]) > 99:
+                print i
+                var= sum(expvar[:i])
+                ncomponent=i
+                break
         
+        if ncp:
+            ncomponent=ncp
+        
+    
+        return X[:,0:ncomponent],ncomponent    
+
+
     def load_process_data(self):
+        """
+        output: 
+            -data1: dataframe for the first videoc 'STIP' points
+            -data2: dataframe for the second video 'STIP' points
+            -hoghof: descriptor of both videos. We used a combination of  histogram of gradients and histogram of flow 
+        """
         
         pathVid1="\\Automatic-measure-beta\\Data\\"+self.vid1Name+".avi"
         pathVid2="\\Automatic-measure-beta\\Data\\"+self.vid2Name+".avi"
@@ -62,9 +89,43 @@ class Imitation:
             os.system(self.projectPath+Imitation.pathSTIP+" -f "+self.projectPath+pathVid2+" -o "+self.projectPath+Imitation.pathout2+ self.vid2Name+".txt -vis no")
             
         
-        
+        #loading the STIP into a panda dataframe
         data1=pd.read_csv('../Data/vid-gen1/'+self.vid1Name+'.txt',header=None, sep=r"\s+",skiprows=3)
         data2=pd.read_csv('../Data/vid-gen2/'+self.vid1Name+'.txt',header=None, sep=r"\s+",skiprows=3)
+        
+        #drop useless columns
+        data1.drop(data1.columns[[0,1,2,3,5,6]], axis=1, inplace=True)
+        data2.drop(data2.columns[[0,1,2,3,5,6]], axis=1, inplace=True)
+        
+        #columns names
+        column=["hgf"+str(i) for i in range(162)]
+        column.insert(0,'frame')
+        #naming the columns
+        data1.columns=column
+        data2.columns=column
+        
+        #merging the two dataframes
+        data=data1.append(data2,ignore_index=True)
+        datapca=data.drop(['frame'],axis=1)
+        
+        if self.scale==1:
+            hoghof = preprocessing.scale(datapca)
+            
+        else:
+            #dataframe to numpy without scaling to be usable in sklearn
+            hoghof=datapca.as_matrix(columns=None)
+            
+        return data1,data2,hoghof
+    
+    
+    
+    
+
+            
+        
+        
+        
+        
         
         
         
